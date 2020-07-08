62bf2cfea8eae04680e1a67f3cde1795b4e9ee0e
James Joy <jamesjp296@gmail.com>
2020-07-08 15:08:34 +1200

    Connected to embedded database

--- course-api-data/src/main/java/io/javabrains/springbootstrater/topic/TopicService.java
+++ course-api-data/src/main/java/io/javabrains/springbootstrater/topic/TopicService.java
@@ -3,54 +3,42 @@ package io.javabrains.springbootstrater.topic;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 public class TopicService {
-	
+
 	@Autowired
 	private TopicRepository topicRepository;
 
-	private List<Topic> topics = new ArrayList<>(Arrays.asList(
-			new Topic("Spring Boot","Spring Boot", "Spring Description"),
-			new Topic("Spring","Spring Framework", "Spring Description"),
-			new Topic("Blockchain","Blockchain decentralization", "Blockchain Description")				
-			));
-	
-	public List<Topic> getAllTopics(){
-		
+	private List<Topic> topics = new ArrayList<>(
+			Arrays.asList(new Topic("Spring Boot", "Spring Boot", "Spring Description"),
+					new Topic("Spring", "Spring Framework", "Spring Description"),
+					new Topic("Blockchain", "Blockchain decentralization", "Blockchain Description")));
+
+	public List<Topic> getAllTopics() {
 		List<Topic> topics = new ArrayList<>();
-		topicRepository.findAll().forEach(topics ::add);
-		return topics; 
+		topicRepository.findAll().forEach(topics::add);
+		return topics;
 	}
-	
+
 	public Topic getTopic(String id) {
-		
-		  return topics.stream().filter(t -> t.getId().equals(id)).findFirst().get();
-		 
-		/* Topic t = topicRepository; */
-			/* return t; */
+		// return topics.stream().filter(t -> t.getId().equals(id)).findFirst().get();
+		return topicRepository.findById(id).get();
 	}
-	
+
 	public void addTopic(Topic topic) {
-		/* topics.add(topic); */
 		topicRepository.save(topic);
 	}
 
 	public void updateTopic(String id, Topic topic) {
-		for(int i=0;i< topics.size(); i++) {
-			Topic t = topics.get(i);
-			if(t.getId().equals(id)) {
-				topics.set(i, topic);
-				return;
-			}
-		}
-		
+		topicRepository.save(topic);
 	}
 
 	public void deleteTopic(String id) {
-		topics.removeIf(t -> t.getId().equals(id));
+		topicRepository.deleteById(id);
 	}
 }
