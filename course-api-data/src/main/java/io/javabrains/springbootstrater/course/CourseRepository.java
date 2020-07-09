0af3b96b5da17a37678fa3e5c8e80bb23ac4681d
James Joy <jamesjp296@gmail.com>
2020-07-09 18:16:51 +1200

    Added course details

---
+++ course-api-data/src/main/java/io/javabrains/springbootstrater/course/CourseRepository.java
@@ -0,0 +1,10 @@
+package io.javabrains.springbootstrater.course;
+
+import java.util.List;
+
+import org.springframework.data.repository.CrudRepository;
+
+public interface CourseRepository extends CrudRepository<Course, String> {
+
+	public List<Course> findByTopicId(String topicId);
+}
